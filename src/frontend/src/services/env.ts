8b46e01fbd47e616cd9f5de6f49a45685358c86c
Denis <63048913+Tryd0g0lik@users.noreply.github.com>
2024-05-30 23:04:53 +0700

    Update env.ts

--- src/frontend/src/services/env.ts
+++ src/frontend/src/services/env.ts
@@ -1,2 +1,2 @@
-export const APP_TABLE_URL = ((typeof process.env.APP_TABLE_URL).includes('string')) ? process.env.APP_TABLE_URL : 'http://localhost:7070';
+export const APP_TABLE_URL = ((typeof process.env.APP_TABLE_URL).includes('string')) ? process.env.APP_TABLE_URL : 'https://test-table-w5ep.onrender.com';
 export const APP_TABLE_PATHNAME = ((typeof process.env.APP_TABLE_PATHNAME).includes('string')) ? process.env.APP_TABLE_PATHNAME : '';
